f400396ce331c5499e59e5d4eba377096274968e
raghavgupta2412 <raghav.gupta_cs21@gla.ac.in>
2024-07-06 13:13:25 +0530

    kj

--- Apis/UserRoutes.js
+++ Apis/UserRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const genrateToken = require("./JsonWebToken");
 
 router.post("/register", async (req, res) => {
